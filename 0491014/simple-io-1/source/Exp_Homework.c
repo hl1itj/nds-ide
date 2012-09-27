72dc42f2e4f057e253c757837d55d8a33f3407b2
hansung <hansung@hansung-VirtualBox.(none)>
2012-09-27 16:34:12 +0900

    add 0491014 simple-io-1 homework

--- 0491014/simple-io-1/source/Exp_Homework.c
+++ 0491014/simple-io-1/source/Exp_Homework.c
@@ -49,5 +49,45 @@ Exp_1_Homework_A(void)
 void
 Exp_1_Homework_B(void)
 {
+	u16 sw;
+	u8 a_b = TRUE;
+	u8 led_state = 0x01;
+
+	writeb_virtual_io(BARLED1, led_state);
+	writeb_virtual_io(BARLED2, 0);
+	while (1) {
+		sw = NDS_SWITCH();
+		if ((sw == KEY_R) && (led_state != 0x01))
+			led_state = led_state >>1;
+		else if ((sw == KEY_R) && (led_state == 0x01)) {
+			led_state = 0x80;
+			if (a_b)
+				a_b = FALSE;
+			else
+				a_b = TRUE;
+		}
+		if ((sw == KEY_L) && (led_state != 0x80))
+			led_state = led_state <<1;
+		else if ((sw == KEY_L) && (led_state == 0x80)) {
+			led_state = 0x01;
+			if (a_b)
+				a_b = FALSE;
+			else
+				a_b = TRUE;
+		}
+		if (a_b){
+			writeb_virtual_io(BARLED1, led_state);
+			writeb_virtual_io(BARLED2, 0);
+		}
+		else{
+			writeb_virtual_io(BARLED1, 0);
+			writeb_virtual_io(BARLED2, led_state);
+		}
+		if (NDS_SWITCH() & KEY_START)
+			break;
+		vTaskDelay(50);
+	}
+	while (NDS_SWITCH() & KEY_START)
+		vTaskDelay(10);
 
 }
