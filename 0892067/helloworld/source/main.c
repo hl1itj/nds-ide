e38ddc8fc705f16ce9de6aef290ad8757204a2e6
TaeUngSong <yukult400@ubuntu.(none)>
2012-09-28 20:57:09 +0900

    test

--- 0892067/helloworld/source/main.c
+++ 0892067/helloworld/source/main.c
@@ -1,48 +1,14 @@
-/*---------------------------------------------------------------------------------
-
-	$Id: main.cpp,v 1.13 2008-12-02 20:21:20 dovoto Exp $
-
-	Simple console print demo
-	-- dovoto
-
-
----------------------------------------------------------------------------------*/
 #include <nds.h>
 
 #include <stdio.h>
 
-volatile int frame = 0;
-
-//---------------------------------------------------------------------------------
-void Vblank() {
-//---------------------------------------------------------------------------------
-	frame++;
-}
-	
-//---------------------------------------------------------------------------------
-int main(void) {
-//---------------------------------------------------------------------------------
-	touchPosition touchXY;
-
-	irqSet(IRQ_VBLANK, Vblank);
-
+int main(void)
+{
 	consoleDemoInit();
 
-	iprintf("      Hello DS dev'rs\n");
-	iprintf("     \x1b[32mwww.devkitpro.org\n");
-	iprintf("   \x1b[32;1mwww.drunkencoders.com\x1b[39m");
- 
-	while(1) {
+	iprintf("Hello World\n");
 	
-		swiWaitForVBlank();
-		touchRead(&touchXY);
-
-		// print at using ansi escape sequence \x1b[line;columnH 
-		iprintf("\x1b[10;0HFrame = %d",frame);
-		iprintf("\x1b[16;0HTouch x = %04X, %04X\n", touchXY.rawx, touchXY.px);
-		iprintf("Touch y = %04X, %04X\n", touchXY.rawy, touchXY.py);		
-	
-	}
+	while(1);
 
 	return 0;
 }
