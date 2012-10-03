a29064bd1fbe1416c01a33f28657a26073205924
hansung <hansung@hansung-VirtualBox.(none)>
2012-10-03 20:26:08 +0900

    add 0892006 simple-io-1 expriment

--- 0892006/simple-io-1/build/down.h
+++ 0892006/simple-io-1/build/down.h
@@ -8,7 +8,7 @@
 //	+ bitmap lz77 compressed
 //	Total size: 12344 = 12344
 //
-//	Time-stamp: 2012-09-27, 14:44:03
+//	Time-stamp: 2012-10-03, 16:58:42
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.10
 //	( http://www.coranac.com/projects/#grit )
 //
--- 0892006/simple-io-1/source/main.c
+++ 0892006/simple-io-1/source/main.c
@@ -68,8 +68,8 @@ portTASK_FUNCTION(Exp_1_Task, pvParameters )
 		//Exp_1_Sample_A();
 		//Exp_1_Sample_B();
 
-		Exp_1_Homework_A();
-	//	Exp_1_Homework_B();
+		//Exp_1_Homework_A();
+		Exp_1_Homework_B();
 	}
 }
 
