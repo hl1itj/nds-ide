d8447f89754d8bdb18f29c9e1370fdd532e119b2
TaeUngSong <yukult400@ubuntu.(none)>
2012-10-04 15:28:17 +0900

    First Homework B

--- 0892067/simple-io-1/build/gdbStubAsm.d
+++ 0892067/simple-io-1/build/gdbStubAsm.d
@@ -1,2 +1,2 @@
 gdbStubAsm.o: \
- /home/hansung/ndsdev/workspace/simple-io-1/FreeRTOS/NDS/gdbStubAsm.s
+ /home/yukult400/ndsdev/nds-ide/0892067/simple-io-1/FreeRTOS/NDS/gdbStubAsm.s
--- 0892067/simple-io-1/build/port_asm.d
+++ 0892067/simple-io-1/build/port_asm.d
@@ -1,2 +1,2 @@
 port_asm.o: \
- /home/hansung/ndsdev/workspace/simple-io-1/FreeRTOS/NDS/port_asm.s
+ /home/yukult400/ndsdev/nds-ide/0892067/simple-io-1/FreeRTOS/NDS/port_asm.s
