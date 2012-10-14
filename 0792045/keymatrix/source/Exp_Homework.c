807772e96cb2592e00b5125ba72063f78cc66277
hansung <hansung@hansung-VirtualBox.(none)>
2012-10-14 22:42:49 +0900

    keymatrix

--- 0792045/keymatrix/source/Exp_Homework.c
+++ 0792045/keymatrix/source/Exp_Homework.c
@@ -7,22 +7,26 @@
 #include "sevencore_io.h"
 
 void Exp_4_Homework_A(void) {
-	u8 state = TRUE, key, scan = 0;
+	u8 state = TRUE, sw_s = TRUE, key, scan = 0;
 	u8 led[7];
-	int i,j=0;
+	int i, j = 0;
 
 	for (i = 0; i < NUM_7SEG_LED; i++)					// Turn Off All
-						writeb_virtual_io(SEG7LED, 0x80 + (i << 4));
+		writeb_virtual_io(SEG7LED, 0x80 + (i << 4));
 	while (1) {
 
 		switch (readb_virtual_io(PUSH_SW)) {
 		case VIRTUAL_SW_1:
-			j =0;
-			state = ~state;
-			for (i = 0; i < NUM_7SEG_LED; i++)					// Turn Off All
+			if (sw_s) {
+				j = 0;
+				state = ~state;
+				for (i = 0; i < NUM_7SEG_LED; i++)				// Turn Off All
 					writeb_virtual_io(SEG7LED, 0x80 + (i << 4));
+				sw_s = FALSE;
+			}
 			break;
 		default:
+			sw_s = TRUE;
 			break;
 		}
 		if (state == TRUE) {
@@ -30,11 +34,23 @@ void Exp_4_Homework_A(void) {
 			writeb_virtual_io(KEY_MATRIX, 0x80 >> scan);
 			key = scan * 4;
 			switch (readb_virtual_io(KEY_MATRIX)) {
-			case 8:key += 1;break;
-			case 4:key += 2;break;
-			case 2:key += 3;break;
-			case 1:key += 4;if (key == 16)key = 0;break;
-			default:key = 255;break;
+			case 8:
+				key += 1;
+				break;
+			case 4:
+				key += 2;
+				break;
+			case 2:
+				key += 3;
+				break;
+			case 1:
+				key += 4;
+				if (key == 16)
+					key = 0;
+				break;
+			default:
+				key = 255;
+				break;
 			}
 			scan++;
 			if (scan == 4)
@@ -43,36 +59,48 @@ void Exp_4_Homework_A(void) {
 			if (key < 16) {
 				led[0] = key;
 				for (i = 0; i < j; i++)
-					writeb_virtual_io(SEG7LED, led[i]+((0x10)*i));
+					writeb_virtual_io(SEG7LED, led[i] + ((0x10) * i));
 				for (i = 7; i > 0; i--)
-					led[i] = led [i-1];
+					led[i] = led[i - 1];
 			}
-			if(j != 8 && key != 255)
+			if (j != 8 && key != 255)
 				j++;
 		} else {
 			//writeb_virtual_io(SEG7LED, 0x02);
 			writeb_virtual_io(KEY_MATRIX, 0x80 >> scan);
-						key = scan * 4;
-						switch (readb_virtual_io(KEY_MATRIX)) {
-						case 8:key += 1;break;
-						case 4:key += 2;break;
-						case 2:key += 3;break;
-						case 1:key += 4;if (key == 16)key = 0;break;
-						default:key = 255;break;
-						}
-						scan++;
-						if (scan == 4)
-							scan = 0;
+			key = scan * 4;
+			switch (readb_virtual_io(KEY_MATRIX)) {
+			case 8:
+				key += 1;
+				break;
+			case 4:
+				key += 2;
+				break;
+			case 2:
+				key += 3;
+				break;
+			case 1:
+				key += 4;
+				if (key == 16)
+					key = 0;
+				break;
+			default:
+				key = 255;
+				break;
+			}
+			scan++;
+			if (scan == 4)
+				scan = 0;
 
-						if (key < 16) {
-							led[0] = key+0x70;
-							for (i = 0; i < j; i++)
-								writeb_virtual_io(SEG7LED, led[i]-((0x10)*i));
-							for (i = 7; i > 0; i--)
-								led[i] = led [i-1];
-						}
-						if(j != 8 && key != 255)
-							j++;
+			if (key < 16) {
+				led[0] = key + 0x70;
+				for (i = 0; i < j; i++)
+					writeb_virtual_io(SEG7LED, led[i] - ((0x10) * i));
+				for (i = 7; i > 0; i--)
+					led[i] = led[i - 1];
+			}
+			if (j != 8 && key != 255)
+				j++;
 		}
 
 		if (NDS_SWITCH() & KEY_START)
