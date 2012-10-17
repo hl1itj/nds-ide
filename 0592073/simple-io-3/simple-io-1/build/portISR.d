5e85723130d02527a5f9e3da054ceb14bc191111
hansung <hansung@hansung-VirtualBox.(none)>
2012-10-17 20:06:15 +0900

    0592073 simple-io-3 complete

---
+++ 0592073/simple-io-3/simple-io-1/build/portISR.d
@@ -0,0 +1,29 @@
+portISR.o: \
+ /home/hansung/ndsdev/workspace/simple-io-1/FreeRTOS/NDS/portISR.c \
+ /home/hansung/ndsdev/workspace/simple-io-1/FreeRTOS/include/FreeRTOS.h \
+ /home/hansung/ndsdev/workspace/simple-io-1/FreeRTOS/include/projdefs.h \
+ /home/hansung/ndsdev/workspace/simple-io-1/FreeRTOS/NDS/FreeRTOSConfig.h \
+ /home/hansung/ndsdev/workspace/simple-io-1/FreeRTOS/include/portable.h \
+ /home/hansung/ndsdev/workspace/simple-io-1/FreeRTOS/NDS/portmacro.h \
+ /home/hansung/ndsdev/workspace/simple-io-1/FreeRTOS/include/mpu_wrappers.h \
+ /home/hansung/ndsdev/workspace/simple-io-1/FreeRTOS/include/task.h \
+ /home/hansung/ndsdev/workspace/simple-io-1/FreeRTOS/include/list.h \
+ /home/hansung/ndsdev/workspace/simple-io-1/FreeRTOS/NDS/ndsreg.h
+
+/home/hansung/ndsdev/workspace/simple-io-1/FreeRTOS/include/FreeRTOS.h:
+
+/home/hansung/ndsdev/workspace/simple-io-1/FreeRTOS/include/projdefs.h:
+
+/home/hansung/ndsdev/workspace/simple-io-1/FreeRTOS/NDS/FreeRTOSConfig.h:
+
+/home/hansung/ndsdev/workspace/simple-io-1/FreeRTOS/include/portable.h:
+
+/home/hansung/ndsdev/workspace/simple-io-1/FreeRTOS/NDS/portmacro.h:
+
+/home/hansung/ndsdev/workspace/simple-io-1/FreeRTOS/include/mpu_wrappers.h:
+
+/home/hansung/ndsdev/workspace/simple-io-1/FreeRTOS/include/task.h:
+
+/home/hansung/ndsdev/workspace/simple-io-1/FreeRTOS/include/list.h:
+
+/home/hansung/ndsdev/workspace/simple-io-1/FreeRTOS/NDS/ndsreg.h:
