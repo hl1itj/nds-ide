c3e7eb9f468866f1c7f8afbcfa2bec400a7d596a
hansung <hansung@hansung-VirtualBox.(none)>
2012-11-01 16:03:14 +0900

    add 0892054 keymatric

--- 0892054/keymatrix/source/Exp_Homework.c
+++ 0892054/keymatrix/source/Exp_Homework.c
@@ -6,23 +6,44 @@
 #include <stdlib.h>
 #include "sevencore_io.h"
 
+
+
 void
 Exp_4_Homework_A(void)
 {
-    // Variables
-    u8 key, scan = 0;
+	enum {NOPUSH,PUSH};
+    u8  key;
+    u8  scan = 0;
+    u8  bKey[4] = {255,255,255,255};
+    u8  keyArr[8];
+    u8  sw1 = FALSE;
+    u8  keyPressd = FALSE;
     int i;
-    int check = 0x70;
-    int cnt=0;
-    u8 key_arr[8]={0,0,0,0,0,0,0,0};
+    int cnt = 0;
 
 
+
+    for (i=0; i<8; i++)
+    	keyArr[i] = 0x80;
+
+	for (i=0; i<NUM_7SEG_LED; i++)					// Turn Off All
+		writeb_virtual_io(SEG7LED, 0x80 + (i << 4));
 
 	while (1) {
-		if(readb_virtual_io(PUSH_SW) == VIRTUAL_SW_1) {
-			for (i = 0; i < NUM_7SEG_LED; i++)					// Turn Off All
-				writeb_virtual_io(SEG7LED, 0x80 + (i << 4));
+		if (readb_virtual_io(PUSH_SW) == VIRTUAL_SW_1){
+			if(!keyPressd){
+				keyPressd =TRUE;
+				if(sw1 == TRUE)
+					sw1 = FALSE;
+				else if(sw1 == FALSE)
+					sw1 = TRUE;
+				for (i=0; i<8; i++)
+					keyArr[i] = 0x80;
+			}
 		}
+		else
+			keyPressd = FALSE;
+
 
 		writeb_virtual_io(KEY_MATRIX, 0x80 >> scan);
 		key = scan * 4;
@@ -36,21 +57,34 @@ Exp_4_Homework_A(void)
 		scan++;
 		if (scan == 4)
 			scan = 0;
+
 		if (key < 16){
-			key_arr[cnt]=key;
-			cnt++;
-			for(i = 0; i <8; i++){
-				writeb_virtual_io(SEG7LED, check + key_arr[i]);
-				check += 0x80;
+			if(key != bKey[scan]){
+				keyArr[cnt%8] = key;
+				cnt++;
+				bKey[scan] = key;
+			}
+		}
+		else
+			bKey[scan] = key;
+
+		if(sw1 == FALSE){
+			for(i=0; i<8; i++){
+				writeb_virtual_io(SEG7LED, 0x00+(0x10*i) + keyArr[(cnt+i)%8]);
+			}
+		}
+		else if(sw1 == TRUE){
+			for(i=0; i<8; i++){
+				writeb_virtual_io(SEG7LED, 0x70-(0x10*i) + keyArr[(cnt+i)%8] );
 			}
 		}
 
-
 		if (NDS_SWITCH() & KEY_START)
 			break;
+
 		vTaskDelay(MSEC2TICK(30));
 	}
-	while (NDS_SWITCH() & KEY_START)
-		vTaskDelay(MSEC2TICK(10));		// Wait while START KEY is being pressed
+	while (NDS_SWITCH() & KEY_START){
+		vTaskDelay(MSEC2TICK(10));	// Wait for releasing START KEY
+	}
 }
-
---
+++ 0892054/keyqueue/build/list.d
@@ -0,0 +1,22 @@
+list.o: /home/hansung/ndsdev/nds-ide/0892054/keyqueue/FreeRTOS/list.c \
+ /home/hansung/ndsdev/nds-ide/0892054/keyqueue/FreeRTOS/include/FreeRTOS.h \
+ /home/hansung/ndsdev/nds-ide/0892054/keyqueue/FreeRTOS/include/projdefs.h \
+ /home/hansung/ndsdev/nds-ide/0892054/keyqueue/FreeRTOS/NDS/FreeRTOSConfig.h \
+ /home/hansung/ndsdev/nds-ide/0892054/keyqueue/FreeRTOS/include/portable.h \
+ /home/hansung/ndsdev/nds-ide/0892054/keyqueue/FreeRTOS/NDS/portmacro.h \
+ /home/hansung/ndsdev/nds-ide/0892054/keyqueue/FreeRTOS/include/mpu_wrappers.h \
+ /home/hansung/ndsdev/nds-ide/0892054/keyqueue/FreeRTOS/include/list.h
+
+/home/hansung/ndsdev/nds-ide/0892054/keyqueue/FreeRTOS/include/FreeRTOS.h:
+
+/home/hansung/ndsdev/nds-ide/0892054/keyqueue/FreeRTOS/include/projdefs.h:
+
+/home/hansung/ndsdev/nds-ide/0892054/keyqueue/FreeRTOS/NDS/FreeRTOSConfig.h:
+
+/home/hansung/ndsdev/nds-ide/0892054/keyqueue/FreeRTOS/include/portable.h:
+
+/home/hansung/ndsdev/nds-ide/0892054/keyqueue/FreeRTOS/NDS/portmacro.h:
+
+/home/hansung/ndsdev/nds-ide/0892054/keyqueue/FreeRTOS/include/mpu_wrappers.h:
+
+/home/hansung/ndsdev/nds-ide/0892054/keyqueue/FreeRTOS/include/list.h:
