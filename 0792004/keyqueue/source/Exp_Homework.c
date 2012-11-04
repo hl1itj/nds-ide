266646b6bd39cdbbe1a1288cf1c505b7e1d95e12
hansung <hansung@hansung-VirtualBox.(none)>
2012-11-04 15:52:54 +0900

    0792004 add KeyQueue Experiment

--- 0792004/keyqueue/source/Exp_Homework.c
+++ 0792004/keyqueue/source/Exp_Homework.c
@@ -8,11 +8,12 @@
 #include "sevencore_io.h"
 
 extern xQueueHandle KeyQueue;
-#define MAX_KEY_LOG        10
+#define MAX_KEY_LOG        	10
 #define KEY_0               0
 #define MIN_LED             0x01
 #define MAX_LED             0x80
-#define SEG7LED_ON(x)     writeb_virtual_io(SEG7LED, (x))
+#define LED_DELAY				500
+#define SEG7LED_ON(x)     	writeb_virtual_io(SEG7LED, x)
 #define LED1(x)             writeb_virtual_io(BARLED1, x)
 
 void
@@ -58,7 +59,7 @@ Exp_5_Homework_A(void)
 	while(1) {
 
 		key = getkey();
-		if (key == 0) break;
+		if (key == KEY_0) break;
 
 		if (cnt == NUM_7SEG_LED) {
 			for(i = 0; i < cnt - 1; i++)
@@ -80,37 +81,40 @@ Exp_5_Homework_B(void)
 {
 	int i;
 	int cnt = 0;
+	int tick_count = LED_DELAY;
+	int tick_time = LED_DELAY / 500;
 	u8 key_arr[NUM_7SEG_LED];
 	u8 key;
 	u8 sw = FALSE;
 	u8 barled = MAX_LED;
+
+	portTickType xLastWakeTime = xTaskGetTickCount();
 
 	for (i = 0; i < NUM_7SEG_LED; i++)    // Turn Off All
 		SEG7LED_ON(0x80 + (i << 4));
 
 	key_init();
 
-	portTickType xLastWakeTime = xTaskGetTickCount();
-
 	while (1) {
-		LED1(barled);
+		if (tick_count == LED_DELAY) {
+			tick_count = 0;
+			LED1(barled);
 
-		if (sw == FALSE)
-			if (barled > MIN_LED)
-				barled /= 2;
-			else {
-				barled *= 2;
-				sw = TRUE;
-			}
-		else
-			if (barled < MAX_LED)
-				barled *= 2;
-			else {
-				barled /= 2;
-				sw = FALSE;
-			}
-		vTaskDelayUntil(&xLastWakeTime, MSEC2TICK(500));
-
+			if (sw == FALSE)
+				if (barled > MIN_LED)
+					barled /= 2;
+				else {
+					barled *= 2;
+					sw = TRUE;
+				}
+			else
+				if (barled < MAX_LED)
+					barled *= 2;
+				else {
+					barled /= 2;
+					sw = FALSE;
+				}
+		}
 		if (kbhit()) {
 			key = getkey();
 
@@ -130,6 +134,9 @@ Exp_5_Homework_B(void)
 
 		if (NDS_SWITCH() & KEY_START)
 			break;
+
+		tick_count += tick_time;
+		vTaskDelayUntil(&xLastWakeTime, MSEC2TICK(tick_time));
 	}
 }
 
@@ -139,9 +146,11 @@ portTASK_FUNCTION(Key_Task, pvParameters)
 	u8 key, scan = 0;
 	u8 key_pressed = FALSE;
 	u8 pre_line;
+	int i;
 
 	while (1) {
 
+		//for (i = 0; i < 100000; i++);
 		// Fill Here
 
 		if (!key_pressed) {
@@ -171,6 +180,6 @@ portTASK_FUNCTION(Key_Task, pvParameters)
 				&& ((readb_virtual_io(KEY_MATRIX) == 0)
 						|| (readb_virtual_io(KEY_MATRIX) != pre_line)))
 			key_pressed = FALSE;
-		vTaskDelay(MSEC2TICK(10));
+		vTaskDelay(MSEC2TICK(25));
 	}
 }
