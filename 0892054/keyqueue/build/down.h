93c23a585e0ec582378781badf8946e4490e81b6
hansung <hansung@hansung-VirtualBox.(none)>
2012-11-08 16:15:00 +0900

    add 0892054 keyqueue

--- 0892054/keyqueue/build/down.h
+++ 0892054/keyqueue/build/down.h
@@ -8,7 +8,7 @@
 //	+ bitmap lz77 compressed
 //	Total size: 12344 = 12344
 //
-//	Time-stamp: 2012-11-01, 15:57:27
+//	Time-stamp: 2012-11-08, 15:18:05
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.10
 //	( http://www.coranac.com/projects/#grit )
 //
