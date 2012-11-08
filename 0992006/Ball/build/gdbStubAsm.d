514fed862094cf691e86332aee382ba18111803c
hansung <hansung@hansung-VirtualBox.(none)>
2012-11-08 16:27:48 +0900

    addnew

---
+++ 0992006/Ball/build/gdbStubAsm.d
@@ -0,0 +1,2 @@
+gdbStubAsm.o: \
+ /home/hansung/ndsdev/nds-ide/0992006/Ball/FreeRTOS/NDS/gdbStubAsm.s
---
+++ 0992006/Ball/build/tasks.d
@@ -0,0 +1,28 @@
+tasks.o: /home/hansung/ndsdev/nds-ide/0992006/Ball/FreeRTOS/tasks.c \
+ /home/hansung/ndsdev/nds-ide/0992006/Ball/FreeRTOS/include/FreeRTOS.h \
+ /home/hansung/ndsdev/nds-ide/0992006/Ball/FreeRTOS/include/projdefs.h \
+ /home/hansung/ndsdev/nds-ide/0992006/Ball/FreeRTOS/NDS/FreeRTOSConfig.h \
+ /home/hansung/ndsdev/nds-ide/0992006/Ball/FreeRTOS/include/portable.h \
+ /home/hansung/ndsdev/nds-ide/0992006/Ball/FreeRTOS/NDS/portmacro.h \
+ /home/hansung/ndsdev/nds-ide/0992006/Ball/FreeRTOS/include/mpu_wrappers.h \
+ /home/hansung/ndsdev/nds-ide/0992006/Ball/FreeRTOS/include/task.h \
+ /home/hansung/ndsdev/nds-ide/0992006/Ball/FreeRTOS/include/list.h \
+ /home/hansung/ndsdev/nds-ide/0992006/Ball/FreeRTOS/include/StackMacros.h
+
+/home/hansung/ndsdev/nds-ide/0992006/Ball/FreeRTOS/include/FreeRTOS.h:
+
+/home/hansung/ndsdev/nds-ide/0992006/Ball/FreeRTOS/include/projdefs.h:
+
+/home/hansung/ndsdev/nds-ide/0992006/Ball/FreeRTOS/NDS/FreeRTOSConfig.h:
+
+/home/hansung/ndsdev/nds-ide/0992006/Ball/FreeRTOS/include/portable.h:
+
+/home/hansung/ndsdev/nds-ide/0992006/Ball/FreeRTOS/NDS/portmacro.h:
+
+/home/hansung/ndsdev/nds-ide/0992006/Ball/FreeRTOS/include/mpu_wrappers.h:
+
+/home/hansung/ndsdev/nds-ide/0992006/Ball/FreeRTOS/include/task.h:
+
+/home/hansung/ndsdev/nds-ide/0992006/Ball/FreeRTOS/include/list.h:
+
+/home/hansung/ndsdev/nds-ide/0992006/Ball/FreeRTOS/include/StackMacros.h:
