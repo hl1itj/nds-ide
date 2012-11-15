3cc0e28e823aacd99a5ed9dd5e83d540abab407a
hansung <hansung@hansung-VirtualBox.(none)>
2012-11-15 15:18:42 +0900

    add

--- 0491014/keyqueue/build/up.h
+++ 0491014/keyqueue/build/up.h
@@ -8,7 +8,7 @@
 //	+ bitmap lz77 compressed
 //	Total size: 11948 = 11948
 //
-//	Time-stamp: 2012-11-08, 16:27:02
+//	Time-stamp: 2012-11-15, 15:09:37
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.10
 //	( http://www.coranac.com/projects/#grit )
 //
--- 0491014/keyqueue/source/Exp_Homework.c
+++ 0491014/keyqueue/source/Exp_Homework.c
@@ -24,7 +24,7 @@ int
 kbhit(void)
 {
 	u8 key;
-	int ret = xQueuePeek(KeyQueue, &key, 0);
+	int ret = xQueuePeek(KeyQueue, &key, 1);
 	return (ret == pdPASS);
 }
 u8
@@ -40,8 +40,10 @@ getkey(void)
 void
 Exp_5_Homework_A(void)
 {
-    u8 key[8];
+    key_init();
+	u8 key[8];
     u8 key_dump;
+	writeb_virtual_io(BARLED1, 0x00);
 
     int i;
     // Variables
@@ -62,9 +64,9 @@ Exp_5_Homework_A(void)
 
 		for (i = 0; i < 8; i++) {
 			if (key[i] < 16)
-				writeb_virtual_io(SEG7LED, (0x00 + (0x01<<4)*i) + key[i]);
+				writeb_virtual_io(SEG7LED, (0x70 - (0x01<<4)*i) + key[i]);
 			else
-				writeb_virtual_io(SEG7LED, 0x80 + (i << 4));
+				writeb_virtual_io(SEG7LED, 0x80 + ((8-1-i) << 4));
 		}
 	}
 }
@@ -72,46 +74,53 @@ Exp_5_Homework_A(void)
 void
 Exp_5_Homework_B(void)
 {
+    key_init();
     u8 key[8];
     u8 key_dump;
-	u8 barled = 0x01;
-	portTickType xLastWakeTime = xTaskGetTickCount();
+    u8 barled = 0x80;
+    u8 left_right = FALSE;
+    portTickType xLastWakeTime = xTaskGetTickCount();
     int i;
     // Variables
 
     for (i = 0; i < 8 ; i++)
  		key[i] = 20;
 
-	writeb_virtual_io(BARLED1, barled);
-
 	while (1) {
-
-
 
 		if (kbhit()) {
 			key_dump = getkey();
-
+	    	if (key_dump == 200)
+	    		break;
 			if (key_dump < 16) {
 				for (i = 7; i > 0 ; i--) key[i] = key[i-1];
 				key[0] = key_dump;
 			}
+		}
 
-			for (i = 0; i < 8; i++) {
-				if (key[i] < 16)
-					writeb_virtual_io(SEG7LED, (0x00 + (0x01<<4)*i) + key[i]);
-				else
-					writeb_virtual_io(SEG7LED, 0x80 + (i << 4));
-			}
-		} else {
+		if ((xLastWakeTime+MSEC2TICK(500))<xTaskGetTickCount()) {
+			xLastWakeTime = xTaskGetTickCount();
 			if (barled == 0x80)
-				barled = 0x01;
+				left_right = TRUE;
+			if (barled == 0x01)
+				left_right = FALSE;
+			if(left_right)
+				barled = barled >> 1;
 			else
 				barled = barled << 1 ;
-
-			writeb_virtual_io(BARLED1, barled);
-			vTaskDelayUntil(&xLastWakeTime, MSEC2TICK(500));
 		}
+
+		for (i = 0; i < 8; i++) {
+			if (key[i] < 16)
+				writeb_virtual_io(SEG7LED, (0x70 - (0x01<<4)*i) + key[i]);
+			else
+				writeb_virtual_io(SEG7LED, 0x80 + ((8-1-i) << 4));
+		}
+
+		writeb_virtual_io(BARLED1, barled);
+
 	}
+
 }
 
 portTASK_FUNCTION(Key_Task, pvParameters)
@@ -124,6 +133,10 @@ portTASK_FUNCTION(Key_Task, pvParameters)
 
 
     while (1) {
+		if (NDS_SWITCH() & KEY_START){
+			key_dump = 200;
+			xQueueSend(KeyQueue, &key_dump, 0);
+		}
 
 		if (key_pressed == FALSE) {
 			writeb_virtual_io(KEY_MATRIX, 0x80 >> scan);
@@ -141,12 +154,13 @@ portTASK_FUNCTION(Key_Task, pvParameters)
 
 			if (key_dump < 16) {
 				key_pressed = TRUE;
-				xQueueSend(KeyQueue, &key_dump, portMAX_DELAY);
-				vTaskDelay(MSEC2TICK(20));
+				xQueueSend(KeyQueue, &key_dump, 0);
 
 			}
 		}
 
+		vTaskDelay(MSEC2TICK(20));
 		if (key_pressed && (readb_virtual_io(KEY_MATRIX) != key_temp))	key_pressed = FALSE;
+
     }
 }
